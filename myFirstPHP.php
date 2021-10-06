671ab514c489c609f2f2cbace44be8bf654a1eab
uggy <n00201661@iadt.ie>
2021-10-07 00:51:12 +0100

    i added a link

---
+++ myFirstPHP.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+
+<h1>My XXXX page</h1>
+
+<?php
+$txt1 = "HELLO PHP";
+$txt2 = "I have changed myFirstPHP too";
+$x = 100;
+$y = 200;
+
+// Line 12 is nogitt the problem. It looks fine, so check above... 
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+?>
+
+<a href="index.php">Click here to go to another page</a>
+
+    
+<!-- now put a link here to get back to the first page -->
+<?php
+    echo "Now can you add another link to this page that links back to the first page";
+?>
+    
+</body>
+</html>
